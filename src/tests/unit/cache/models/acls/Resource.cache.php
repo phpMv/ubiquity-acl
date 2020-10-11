b78dbc9e802b0199be33a4abe8d91df5ce8594fc
jcheron <myaddressmail@gmail.com>
2020-10-11 18:16:57 +0200

    fix case pbs

--- src/tests/unit/cache/models/acls/Resource.cache.php
+++ src/tests/unit/cache/models/acls/Resource.cache.php
@@ -1,2 +1,43 @@
 <?php
-return array("#tableName"=>"resource","#primaryKeys"=>array("id"=>"id"),"#manyToOne"=>array(),"#fieldNames"=>array("value"=>"value","id"=>"id","name"=>"name","aclelements"=>"aclelements"),"#memberNames"=>array("value"=>"value","id"=>"id","name"=>"name","aclelements"=>"aclelements"),"#fieldTypes"=>array("value"=>"varchar(30)","id"=>"int(11)","name"=>"varchar(30)","aclelements"=>"mixed"),"#nullable"=>array(),"#notSerializable"=>array("aclelements"),"#transformers"=>array(),"#accessors"=>array("value"=>"setValue","id"=>"setId","name"=>"setName","aclelements"=>"setAclelements"),"#oneToMany"=>array("aclelements"=>array("mappedBy"=>"resource","className"=>"models\\acls\\Aclelement")));
+return array(
+	"#tableName" => "Resource",
+	"#primaryKeys" => array(
+		"id" => "id"
+	),
+	"#manyToOne" => array(),
+	"#fieldNames" => array(
+		"value" => "value",
+		"id" => "id",
+		"name" => "name",
+		"aclelements" => "aclelements"
+	),
+	"#memberNames" => array(
+		"value" => "value",
+		"id" => "id",
+		"name" => "name",
+		"aclelements" => "aclelements"
+	),
+	"#fieldTypes" => array(
+		"value" => "varchar(30)",
+		"id" => "int(11)",
+		"name" => "varchar(30)",
+		"aclelements" => "mixed"
+	),
+	"#nullable" => array(),
+	"#notSerializable" => array(
+		"aclelements"
+	),
+	"#transformers" => array(),
+	"#accessors" => array(
+		"value" => "setValue",
+		"id" => "setId",
+		"name" => "setName",
+		"aclelements" => "setAclelements"
+	),
+	"#oneToMany" => array(
+		"aclelements" => array(
+			"mappedBy" => "resource",
+			"className" => "models\\acls\\Aclelement"
+		)
+	)
+);
