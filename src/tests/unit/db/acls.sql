02b28146aa633dc2f3115f83e03bff47adda5a7f
jcheron <myaddressmail@gmail.com>
2020-10-11 19:00:29 +0200

    Update acls.sql

--- src/tests/unit/db/acls.sql
+++ src/tests/unit/db/acls.sql
@@ -18,3 +18,49 @@ ALTER TABLE `AclElement` ADD KEY (`idResource`);
 ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Role` FOREIGN KEY (`idRole`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Permission` FOREIGN KEY (`idPermission`) REFERENCES `Permission` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Resource` FOREIGN KEY (`idResource`) REFERENCES `Resource` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Déchargement des données de la table `aclelement`
+--
+
+INSERT INTO `AclElement` (`id`, `idRole`, `idPermission`, `idResource`) VALUES
+(1, 1, 1, 1);
+
+--
+-- Déchargement des données de la table `permission`
+--
+
+INSERT INTO `Permission` (`level`, `id`, `name`) VALUES
+(1, 1, 'READ'),
+(2, 2, 'WRITE');
+
+--
+-- Déchargement des données de la table `resource`
+--
+
+INSERT INTO `Resource` (`value`, `id`, `name`) VALUES
+('/home', 1, 'Home'),
+('/admin', 2, 'Admin');
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `Role` (`parents`, `id`, `name`) VALUES
+('', 1, 'USER'),
+('USER', 2, 'ADMIN');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
