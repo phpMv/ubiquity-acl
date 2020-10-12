4b33bb214bfabd60d09218db17e7ec7aa2be0a96
jcheron <myaddressmail@gmail.com>
2020-10-12 03:09:03 +0200

    Update AclListOperationsTrait.php

--- src/Ubiquity/security/acl/models/traits/AclListOperationsTrait.php
+++ src/Ubiquity/security/acl/models/traits/AclListOperationsTrait.php
@@ -53,6 +53,9 @@ trait AclListOperationsTrait {
 	public function removeRole(string $roleName) {
 		$role = $this->getRoleByName($roleName);
 		unset($this->roles["role_$roleName"]);
+		if (isset($this->elementsCache["role_$roleName"])) {
+			unset($this->elementsCache["role_$roleName"]);
+		}
 		$this->removeAcl($roleName);
 		$this->removePart($role);
 	}
@@ -60,6 +63,9 @@ trait AclListOperationsTrait {
 	public function removePermission(string $permissionName) {
 		$permission = $this->getPermissionByName($permissionName);
 		unset($this->permissions["perm_$permissionName"]);
+		if (isset($this->elementsCache["perm_$permissionName"])) {
+			unset($this->elementsCache["perm_$permissionName"]);
+		}
 		$this->removeAcl(null, null, $permissionName);
 		$this->removePart($permission);
 	}
@@ -67,6 +73,9 @@ trait AclListOperationsTrait {
 	public function removeResource(string $resourceName) {
 		$resource = $this->getResourceByName($resourceName);
 		unset($this->resources["res_$resourceName"]);
+		if (isset($this->elementsCache["res_$resourceName"])) {
+			unset($this->elementsCache["res_$resourceName"]);
+		}
 		$this->removeAcl(null, $resourceName);
 		$this->removePart($resource);
 	}
