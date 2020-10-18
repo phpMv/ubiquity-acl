1456e5ce7855f97714bd761c870ace3ad3fd11f4
jcheron <myaddressmail@gmail.com>
2020-10-19 01:49:23 +0200

    Update _bootstrap.php

--- src/tests/unit/_bootstrap.php
+++ src/tests/unit/_bootstrap.php
@@ -3,7 +3,7 @@ $loader = include __DIR__ . '/../../../vendor/autoload.php';
 $loader->setPsr4("", __DIR__ . "/app/");
 
 if (! defined('ROOT')) {
-	define('ROOT', __DIR__);
+	define('ROOT', __DIR__ . "/app/");
 }
 if (! defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
