7f119bdac9e00bcd06fc55fde50c9fee7a3b611a
jcheron <myaddressmail@gmail.com>
2021-01-10 04:01:39 +0100

    Update AclControllerParser.php

--- src/Ubiquity/security/acl/cache/AclControllerParser.php
+++ src/Ubiquity/security/acl/cache/AclControllerParser.php
@@ -12,27 +12,27 @@ use Ubiquity\exceptions\AclException;
  * This class is part of Ubiquity
  *
  * @author jc
- * @version 1.0.1
+ * @version 1.0.2
  *
  */
 class AclControllerParser {
-
+	
 	protected $controllerClass;
-
+	
 	protected $mainResource;
-
+	
 	protected $mainPermission;
-
+	
 	protected $permissionMap;
-
+	
 	public function __construct() {
 		$this->permissionMap = new PermissionsMap();
 	}
-
+	
 	public function init() {
 		$this->permissionMap->init();
 	}
-
+	
 	public function parse($controllerClass) {
 		$this->controllerClass = $controllerClass;
 		$reflect = new \ReflectionClass($controllerClass);
@@ -56,7 +56,7 @@ class AclControllerParser {
 			$this->parseMethods($methods);
 		}
 	}
-
+	
 	protected function parseMethods($methods) {
 		$hasPermission = false;
 		$controllerClass = $this->controllerClass;
@@ -79,7 +79,7 @@ class AclControllerParser {
 			}
 		}
 	}
-
+	
 	protected function parseMethod(\ReflectionMethod $method, bool &$hasPermission, $controller) {
 		$action = $method->name;
 		$permission = NULL;
@@ -99,7 +99,7 @@ class AclControllerParser {
 					$hasPermission = true;
 				}
 				$resource ??= $this->mainResource ? $this->mainResource->name : ($controller . '.' . $action);
-
+				
 				$annotsAllow = Reflexion::getAnnotationsMethod($controllerClass, $action, 'allow');
 				if (\is_array($annotsAllow) && \count($annotsAllow) > 0) {
 					$this->addAllows($annotsAllow, $controller, $action, $resource, $permission);
@@ -112,7 +112,7 @@ class AclControllerParser {
 			}
 		}
 	}
-
+	
 	protected function addAllows($annotsAllow, $controller, $action, &$resource, &$permission) {
 		foreach ($annotsAllow as $annotAllow) {
 			if (isset($annotAllow->resource) && isset($resource) && \strpos($resource, '.') === FALSE && $resource !== $annotAllow->resource && $permission != null) {
@@ -121,11 +121,10 @@ class AclControllerParser {
 			if (isset($annotAllow->permission) && isset($permission) && $permission !== $annotAllow->permission) {
 				throw new AclException("Permissions {$permission} and {$annotAllow->permission} are in conflict for action {$controller}.{$action}");
 			}
-			if ($permission === null && $annotAllow->permission === null) {
-				$resource = $controller . '.' . $action;
-			}
-			$resource = $annotAllow->resource ?? $resource;
-			$permission = $annotAllow->permission ?? $permission;
+			
+			$resource = ($annotAllow->resource ?? $resource)??$controller . '.' . $action;
+			$permission = ($annotAllow->permission ?? $permission)??\strtoupper($action);
+			
 			if (\is_array($annotAllow->role)) {
 				foreach ($annotAllow->role as $role) {
 					AclManager::addAndAllow($role, $resource, $permission);
@@ -135,7 +134,7 @@ class AclControllerParser {
 			}
 		}
 	}
-
+	
 	public function save() {
 		$this->permissionMap->save();
 		AclManager::saveAll();
