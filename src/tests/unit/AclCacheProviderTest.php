99237c6036c7afde13e6c761fa38ad5321e9803f
jcheron <myaddressmail@gmail.com>
2021-10-08 00:44:01 +0200

    Update AclCacheProviderTest.php

--- src/tests/unit/AclCacheProviderTest.php
+++ src/tests/unit/AclCacheProviderTest.php
@@ -133,7 +133,7 @@ class AclCacheProviderTest extends \Codeception\Test\Unit {
 	 */
 	public function testInitCache() {
 		$this->initTestController();
-		$this->assertEquals(3, count(AclManager::getRoles()));
+		$this->assertEquals(2, count(AclManager::getRoles()));
 		$this->assertEquals(3, \count(AclManager::getAcls()));
 		$this->assertEquals(4, count(AclManager::getResources()));
 		$this->assertEquals(7, count(AclManager::getPermissions()));
