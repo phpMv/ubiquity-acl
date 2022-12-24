1b3790d514bc23ac9cf35f7d2d25d59b6b8e297f
jcheron <myaddressmail@gmail.com>
2022-12-24 14:12:54 +0100

    update models cache

--- src/tests/unit/app/cache/models/acls/Aclelement.cache.php
+++ src/tests/unit/app/cache/models/acls/Aclelement.cache.php
@@ -1,74 +1,14 @@
 <?php
 return array(
-	"#tableName" => "AclElement",
-	"#primaryKeys" => array(
-		"id" => "id"
-	),
-	"#manyToOne" => array(
-		"permission",
-		"resource",
-		"role"
-	),
-	"#fieldNames" => array(
-		"id" => "id",
-		"permission" => "idPermission",
-		"resource" => "idResource",
-		"role" => "idRole"
-	),
-	"#memberNames" => array(
-		"id" => "id",
-		"idPermission" => "permission",
-		"idResource" => "resource",
-		"idRole" => "role"
-	),
-	"#fieldTypes" => array(
-		"id" => "int(11)",
-		"permission" => false,
-		"resource" => false,
-		"role" => false
-	),
-	"#nullable" => array(),
-	"#notSerializable" => array(
-		"permission",
-		"resource",
-		"role"
-	),
-	"#transformers" => array(),
-	"#accessors" => array(
-		"id" => "setId",
-		"idPermission" => "setPermission",
-		"idResource" => "setResource",
-		"idRole" => "setRole"
-	),
-	"#joinColumn" => array(
-		"permission" => array(
-			"className" => "models\\acls\\Permission",
-			"name" => "idPermission",
-			"nullable" => false
-		),
-		"resource" => array(
-			"className" => "models\\acls\\Resource",
-			"name" => "idResource",
-			"nullable" => false
-		),
-		"role" => array(
-			"className" => "models\\acls\\Role",
-			"name" => "idRole",
-			"nullable" => false
-		)
-	),
-	"#invertedJoinColumn" => array(
-		"idPermission" => array(
-			"member" => "permission",
-			"className" => "models\\acls\\Permission"
-		),
-		"idResource" => array(
-			"member" => "resource",
-			"className" => "models\\acls\\Resource"
-		),
-		"idRole" => array(
-			"member" => "role",
-			"className" => "models\\acls\\Role"
-		)
-	)
+	"#tableName"=>"aclElement",
+	"#primaryKeys"=>["id"=>"id"],
+	"#manyToOne"=>["role","permission","resource"],
+	"#fieldNames"=>["id"=>"id","role"=>"roleName","permission"=>"permissionName","resource"=>"resourceName"],
+	"#memberNames"=>["id"=>"id","roleName"=>"role","permissionName"=>"permission","resourceName"=>"resource"],
+	"#fieldTypes"=>["id"=>"int(11)","role"=>"mixed","permission"=>"mixed","resource"=>"mixed"],"#nullable"=>["id"],
+	"#notSerializable"=>["role","permission","resource"],
+	"#transformers"=>[],
+	"#accessors"=>["id"=>"setId","roleName"=>"setRole","permissionName"=>"setPermission","resourceName"=>"setResource"],
+	"#joinColumn"=>["role"=>["className"=>"Ubiquity\\security\\acl\\models\\Role","name"=>"roleName"],"permission"=>["className"=>"Ubiquity\\security\\acl\\models\\Permission","name"=>"permissionName"],"resource"=>["className"=>"Ubiquity\\security\\acl\\models\\Resource","name"=>"resourceName"]],
+	"#invertedJoinColumn"=>["roleName"=>["member"=>"role","className"=>"Ubiquity\\security\\acl\\models\\Role"],"permissionName"=>["member"=>"permission","className"=>"Ubiquity\\security\\acl\\models\\Permission"],"resourceName"=>["member"=>"resource","className"=>"Ubiquity\\security\\acl\\models\\Resource"]]
 );
