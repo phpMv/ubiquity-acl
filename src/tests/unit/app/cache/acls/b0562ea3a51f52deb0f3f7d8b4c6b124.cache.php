d7ad73657c23c58b6d2c0625d1f6730bdaa61d62
jcheron <myaddressmail@gmail.com>
2022-12-24 16:28:13 +0100

    [test] update cache files

--- src/tests/unit/app/cache/acls/b0562ea3a51f52deb0f3f7d8b4c6b124.cache.php
+++ src/tests/unit/app/cache/acls/b0562ea3a51f52deb0f3f7d8b4c6b124.cache.php
@@ -1,2 +1,2 @@
 <?php
-return array("ALLOW"=>["level"=>0,"id"=>false,"name"=>"ALLOW"],"ALLOW_OTHER"=>["level"=>0,"id"=>false,"name"=>"ALLOW_OTHER"],"NEW_PERMISSION"=>["level"=>0,"id"=>false,"name"=>"NEW_PERMISSION"],"READ"=>["level"=>0,"id"=>false,"name"=>"READ"],"ALLOWOTHER2"=>["level"=>0,"id"=>false,"name"=>"ALLOWOTHER2"],"ADMIN"=>["level"=>100,"id"=>false,"name"=>"ADMIN"]);
+return array("ALLOW"=>["level"=>0,"name"=>"ALLOW"],"ALLOW_OTHER"=>["level"=>0,"name"=>"ALLOW_OTHER"],"NEW_PERMISSION"=>["level"=>0,"name"=>"NEW_PERMISSION"],"READ"=>["level"=>0,"name"=>"READ"],"ALLOWOTHER2"=>["level"=>0,"name"=>"ALLOWOTHER2"],"ADMIN"=>["level"=>100,"name"=>"ADMIN"]);
