4a3615a494826836a1c3d9f3214360e62f5d3039
jcheron <myaddressmail@gmail.com>
2022-12-24 05:05:58 +0100

    Update acls.sql

--- src/tests/unit/db/acls.sql
+++ src/tests/unit/db/acls.sql
@@ -1,23 +1,20 @@
 CREATE DATABASE `acls`;
 USE `acls`;
-CREATE TABLE `Role` (`parents` varchar(30) NOT NULL,`id` int(11) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `Permission` (`level` int(11) NOT NULL,`id` int(11) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `Resource` (`value` varchar(30) NOT NULL,`id` int(11) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `Role` (`parents` varchar(30) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `Permission` (`level` int(11) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `Resource` (`value` varchar(30) NOT NULL,`name` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `AclElement` (`id` int(11) NOT NULL,`idRole` int(11) NOT NULL,`idPermission` int(11) NOT NULL,`idResource` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `Role` ADD PRIMARY KEY (`id`);
-ALTER TABLE `Role` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-ALTER TABLE `Permission` ADD PRIMARY KEY (`id`);
-ALTER TABLE `Permission` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-ALTER TABLE `Resource` ADD PRIMARY KEY (`id`);
-ALTER TABLE `Resource` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+ALTER TABLE `Role` ADD PRIMARY KEY (`name`);
+ALTER TABLE `Permission` ADD PRIMARY KEY (`name`);
+ALTER TABLE `Resource` ADD PRIMARY KEY (`name`);
 ALTER TABLE `AclElement` ADD PRIMARY KEY (`id`);
 ALTER TABLE `AclElement` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 ALTER TABLE `AclElement` ADD KEY (`idRole`);
 ALTER TABLE `AclElement` ADD KEY (`idPermission`);
 ALTER TABLE `AclElement` ADD KEY (`idResource`);
-ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Role` FOREIGN KEY (`idRole`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Permission` FOREIGN KEY (`idPermission`) REFERENCES `Permission` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Resource` FOREIGN KEY (`idResource`) REFERENCES `Resource` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Role` FOREIGN KEY (`roleName`) REFERENCES `Role` (`name`) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Permission` FOREIGN KEY (`permissionName`) REFERENCES `Permission` (`name`) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE `AclElement` ADD CONSTRAINT `fk_AclElement_Resource` FOREIGN KEY (`resourceName`) REFERENCES `Resource` (`name`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,32 +30,32 @@ SET time_zone = "+00:00";
 -- Déchargement des données de la table `permission`
 --
 
-INSERT INTO `Permission` (`level`, `id`, `name`) VALUES
-(1, 1, 'READ'),
-(2, 2, 'WRITE');
+INSERT INTO `Permission` (`level`, `name`) VALUES
+(1, 'READ'),
+(2, 'WRITE');
 
 --
 -- Déchargement des données de la table `resource`
 --
 
-INSERT INTO `Resource` (`value`, `id`, `name`) VALUES
-('/home', 1, 'Home'),
-('/admin', 2, 'Admin');
+INSERT INTO `Resource` (`value`,`name`) VALUES
+('/home','Home'),
+('/admin','Admin');
 
 --
 -- Déchargement des données de la table `role`
 --
 
-INSERT INTO `Role` (`parents`, `id`, `name`) VALUES
-('', 1, 'USER'),
-('USER', 2, 'ADMIN');
+INSERT INTO `Role` (`parents`, `name`) VALUES
+('','USER'),
+('USER','ADMIN');
 
 --
 -- Déchargement des données de la table `aclelement`
 --
 
-INSERT INTO `AclElement` (`id`, `idRole`, `idPermission`, `idResource`) VALUES
-(1, 1, 1, 1);
+INSERT INTO `AclElement` (`id`, `roleName`, `permissionName`, `resourceName`) VALUES
+(1, 'USER', 'READ', 'Home');
 
 COMMIT;
 
